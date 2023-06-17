20fa6c5b9f10b73e9104dca90c2331bfc31f8a35
kaoutarOA <kaoutar.abdelouassa.cla@gmail.com>
2023-06-17 08:17:10 -0700

    lololo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-int put(const char* "Programming is like building a multilingual puzzle");return 0;
+#include<stdio.h>
+int main()
+{char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return 0;
+}
