0870f53705a3674d967a2197485eb12d3c029482
kaoutarOA <kaoutar.abdelouassa.cla@gmail.com>
2023-06-18 12:08:16 -0700

    lololo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 /**
  * main - A function that print n value
  * Return: 0 (success) 
- *more headers goes there */
-/* betty style doc for function main goes there */
+ *more headers goes there
+ */
 int main(void)
 {
 int n;
@@ -17,7 +17,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else if (n % 10  == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n,n % 10);
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 else if ((n % 10 < 6) && (n % 10 != 0))
 {
